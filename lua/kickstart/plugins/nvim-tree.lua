4f5bb514f2b892a0b0177dbb59a03b53745db78a
Joshua Binns <josh@Joshuas-MacBook-Pro.local>
2026-05-16 16:15:38 +0100

    task: update config for nvim 0.12
    The 0.12 release of `nvim` broke my config. I updated it using the
    latest changes from `kickstart.nvim` and it has come with some nice QoL
    improvements. The only thing I need to check before merging is that the
    typescript formatting issues that I fixed most recently have been
    preserved.

--- lua/kickstart/plugins/nvim-tree.lua
+++ lua/kickstart/plugins/nvim-tree.lua
@@ -1,28 +1,41 @@
-return {
-  'nvim-tree/nvim-tree.lua',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
+vim.pack.add({
+  {
+    src = 'https://github.com/nvim-tree/nvim-tree.lua',
   },
-  lazy = false,
-  keys = {
-    { '\\', ':NvimTreeToggle<CR>', desc = 'NvimTree toggle', silent = true },
+
+  -- dependencies
+  {
+    src = 'https://github.com/nvim-lua/plenary.nvim',
   },
-  opts = {
-    view = {
-      width = 50,
-    },
-    actions = {
-      open_file = {
-        -- NOTE: After working with this turned on for a bit, quit-on-open is a bit overkill
-        quit_on_open = false,
-        resize_window = true,
-      },
-    },
-    update_focused_file = {
-      enable = true,
+  {
+    src = 'https://github.com/nvim-tree/nvim-web-devicons',
+  },
+  {
+    src = 'https://github.com/MunifTanjim/nui.nvim',
+  },
+})
+
+-- keymap
+vim.keymap.set('n', '\\', ':NvimTreeToggle<CR>', {
+  desc = 'NvimTree toggle',
+  silent = true,
+})
+
+require('nvim-tree').setup({
+  view = {
+    width = 50,
+  },
+
+  actions = {
+    open_file = {
+      -- After working with this turned on for a bit,
+      -- quit-on-open is a bit overkill
+      quit_on_open = false,
+      resize_window = true,
     },
   },
-}
+
+  update_focused_file = {
+    enable = true,
+  },
+})
